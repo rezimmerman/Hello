6907bdd037c88e3a60b8b9ff735ceb54d5116028
rezimmerman <54566507+rezimmerman@users.noreply.github.com>
2019-08-27 21:24:54 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stdlib.h> \\Include Library
+#include<stdio.h>  \\Include input/output
+#include<math.h>   \\Basic Mathematical Operations
+
+/**
+* Author: Reid Zimmerman, Ben Finnegan
+* Date: 8/27/2019
+*
+* A simple hello world program in C
+*/
+
+int main(int argc, char **argv)
+
+{
+
+printf("Reid Zimmerman and Ben Finnegan\n");
+return 0;
+
+}
